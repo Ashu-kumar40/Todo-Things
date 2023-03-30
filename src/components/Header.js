034076dcc6c0f3b329ff44586f8234221ffd225d
Ashu Kumar <95637428+Ashu-kumar40@users.noreply.github.com>
2023-03-30 07:48:41 +0000

    Header Component added

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../CSS/header.module.css'
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Your Todo</h1>
+    </div>
+  )
+}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,30 +1,36 @@
 import React, { useState } from 'react'
-import ListItems from './ListItems';
+import Header from './Header';
+import '../CSS/global.css'
+// import ListItems from './ListItems';
 
 export default function Todo() {
-    const [todo, setTodo] = useState("");
-    const [todoList, setTodolist] = useState([]);
 
-    const HandleTodo = (event) => {
-        setTodo(event.target.value);
-    }
-    const HandleSubmit = (event) =>{
-        event.preventDefault();
-        let tempList = todoList;
-        tempList.push(todo);
-        setTodolist(tempList);
-        console.log(todoList);
-        setTodo("");
-    }
+    // const [todo, setTodo] = useState("");
+    // const [todoList, setTodolist] = useState([]);
+
+    // const HandleTodo = (event) => {
+    //     setTodo(event.target.value);
+    // }
+    // const HandleSubmit = (event) =>{
+    //     event.preventDefault();
+    //     let tempList = todoList;
+    //     tempList.push(todo);
+    //     setTodolist(tempList);
+    //     console.log(todoList);
+    //     setTodo("");
+    // }
+    
   return (
     <div>
-      <form onSubmit={HandleSubmit}>
+      <Header />
+      
+      {/* <form onSubmit={HandleSubmit}>
         <input type="text" value={todo} onChange={HandleTodo}/>
         <button type='submit'>Add Todo</button>
       </form>
       {todoList.map((item, index) => (
             <ListItems key={index} name={item} />
-      ))}
+      ))} */}
     </div>
   )
 }
